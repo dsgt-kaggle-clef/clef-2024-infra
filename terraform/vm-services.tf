dd38d47fbfeaeb0ad78217293c9983986c26c503
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2024-03-13 14:31:10 +0100

    Create new instances for running services

---
+++ terraform/vm-services.tf
@@ -0,0 +1,38 @@
+// define a e2-micro instance to run luigi
+resource "google_compute_instance" "services" {
+  name         = "services"
+  machine_type = "e2-micro"
+  zone         = "${local.region}-a"
+  labels = {
+    app = "services"
+  }
+  tags                      = ["http-server", "https-server"]
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      size  = 10
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+  scheduling {
+    automatic_restart  = true
+    provisioning_model = "STANDARD"
+  }
+  service_account {
+    email  = data.google_compute_default_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
+
+output "services-vm" {
+  value = {
+    id          = google_compute_instance.services.id
+    external_ip = google_compute_instance.services.network_interface.0.access_config.0.nat_ip
+  }
+}
